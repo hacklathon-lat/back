66dd623601bab00e78cb33605cc88474b75f3cdd
Luis Guzman <lumigu.dev@gmail.com>
2026-01-07 09:37:20 -0500

    🔧 feat: enhance CORS middleware configuration based on environment settings

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+from core.config import settings
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+if settings.ENVIRONMENT == "development":
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+else:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 @app.get("/")
